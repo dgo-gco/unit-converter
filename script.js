004e6306ace5e24ec03eac6373d623c26828bbaf
Diego <110729317+dgo-gco@users.noreply.github.com>
2022-12-18 11:19:12 +0100

    adding comments

--- script.js
+++ script.js
@@ -20,8 +20,9 @@ convertBtn.addEventListener("click", function(){
     //Writing our answers in a single line with template strings
     metersFeet.innerHTML = `<p>${userValue} meters = ${metersToFeet.toFixed(3)} feet | 
     ${userValue} feet = ${feetToMeters.toFixed(3)} meters</p>`
+    //toFixed() method rounds the string to a specified number of decimals, in this case I assigned: 3.
 
-    let litersToGallons = userValue * units.liter;
+    let litersToGallons = userValue * units.liter; //units.liter accessing the liter number from the object units
     let gallonsToLiters = userValue / units.liter;
     
     litersGallons.innerHTML = `<p>${userValue} liters = ${litersToGallons.toFixed(3)} gallons | 
